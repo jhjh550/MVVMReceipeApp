0857321d86ab081be02e37fca3ad2b1ada65ed6a
jh-4750 <navith@naver.com>
2021-08-27 20:34:22 +0900

    column / row layout 2

--- app/src/main/java/kr/co/mvvmreceipeapp/MainActivity.kt
+++ app/src/main/java/kr/co/mvvmreceipeapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -29,70 +30,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MVVMReceipeAppTheme {
-                Column {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(200.dp)
-                            .border(BorderStroke(width = 1.dp, color = Color.Black)),
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Text(
-                            text = "Item1",
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
-                        )
-                        Text(
-                            text = "Item2",
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
-                        )
-                    }
+                Column (
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0xfff2f2f2))
+                ){
+                    Image(
+                        painterResource(R.drawable.happy_meal_small),
+                        "happy meal",
+                        modifier = Modifier.height(300.dp),
+                        contentScale = ContentScale.Crop
+                    )
 
-                    Spacer(modifier = Modifier.size(20.dp))
-                    Row(
-                        modifier = Modifier
-                            .width(200.dp)
-                            .height(200.dp)
-                            .border(BorderStroke(width = 1.dp, color = Color.Black)),
-                        horizontalArrangement = Arrangement.Center
-                    ) {
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ){
+                            Text(
+                                text = "Happy Meal",
+                                fontSize = 26.sp
+                            )
+                            Spacer(modifier = Modifier.size(10.dp))
+                            Text(
+                                text = "$5.99",
+                                fontSize = 17.sp,
+                                color = Color(0xff85bb65),
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            )
+                        }
                         Text(
-                            text = "Item2",
-                            modifier = Modifier.align(Alignment.CenterVertically)
+                            text = "800 Calories",
+                            fontSize = 17.sp
                         )
+                        Spacer(modifier = Modifier.size(10.dp))
+                        Button(
+                            onClick = {},
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        ){
+                            Text("ORDER NOW")
+                        }
                     }
                 }
-
-
-//                Column (
-//                    modifier = Modifier
-//                        .fillMaxSize()
-//                        .background(Color(0xfff2f2f2))
-//                ){
-//                    Image(
-//                        painterResource(R.drawable.happy_meal_small),
-//                        "happy meal",
-//                        modifier = Modifier.height(300.dp),
-//                        contentScale = ContentScale.Crop
-//                    )
-//
-//                    Column(modifier = Modifier.padding(16.dp)) {
-//                        Text(
-//                            text = "Happy Meal",
-//                            fontSize = 26.sp
-//                        )
-//                        Spacer(modifier = Modifier.size(10.dp))
-//                        Text(
-//                            text = "800 Calories",
-//                            fontSize = 17.sp
-//                        )
-//                        Spacer(modifier = Modifier.size(10.dp))
-//                        Text(
-//                            text = "$5.99",
-//                            fontSize = 17.sp,
-//                            color = Color(0xff85bb65)
-//                        )
-//                    }
-//                }
             }
         }
     }
