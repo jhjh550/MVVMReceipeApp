28c9de79a308b3586d1b7f48760a4473cd9b3a15
jh-4750 <navith@naver.com>
2021-08-27 20:23:08 +0900

    column / row layout

--- app/src/main/java/kr/co/mvvmreceipeapp/MainActivity.kt
+++ app/src/main/java/kr/co/mvvmreceipeapp/MainActivity.kt
@@ -3,14 +3,17 @@ package kr.co.mvvmreceipeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -26,36 +29,70 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MVVMReceipeAppTheme {
-                Column (
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color(0xfff2f2f2))
-                ){
-                    Image(
-                        painterResource(R.drawable.happy_meal_small),
-                        "happy meal",
-                        modifier = Modifier.height(300.dp),
-                        contentScale = ContentScale.Crop
-                    )
+                Column {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                            .border(BorderStroke(width = 1.dp, color = Color.Black)),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Item1",
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                        Text(
+                            text = "Item2",
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                    }
 
-                    Column(modifier = Modifier.padding(16.dp)) {
+                    Spacer(modifier = Modifier.size(20.dp))
+                    Row(
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(200.dp)
+                            .border(BorderStroke(width = 1.dp, color = Color.Black)),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
                         Text(
-                            text = "Happy Meal",
-                            fontSize = 26.sp
-                        )
-                        Spacer(modifier = Modifier.size(10.dp))
-                        Text(
-                            text = "800 Calories",
-                            fontSize = 17.sp
-                        )
-                        Spacer(modifier = Modifier.size(10.dp))
-                        Text(
-                            text = "$5.99",
-                            fontSize = 17.sp,
-                            color = Color(0xff85bb65)
+                            text = "Item2",
+                            modifier = Modifier.align(Alignment.CenterVertically)
                         )
                     }
                 }
+
+
+//                Column (
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .background(Color(0xfff2f2f2))
+//                ){
+//                    Image(
+//                        painterResource(R.drawable.happy_meal_small),
+//                        "happy meal",
+//                        modifier = Modifier.height(300.dp),
+//                        contentScale = ContentScale.Crop
+//                    )
+//
+//                    Column(modifier = Modifier.padding(16.dp)) {
+//                        Text(
+//                            text = "Happy Meal",
+//                            fontSize = 26.sp
+//                        )
+//                        Spacer(modifier = Modifier.size(10.dp))
+//                        Text(
+//                            text = "800 Calories",
+//                            fontSize = 17.sp
+//                        )
+//                        Spacer(modifier = Modifier.size(10.dp))
+//                        Text(
+//                            text = "$5.99",
+//                            fontSize = 17.sp,
+//                            color = Color(0xff85bb65)
+//                        )
+//                    }
+//                }
             }
         }
     }
