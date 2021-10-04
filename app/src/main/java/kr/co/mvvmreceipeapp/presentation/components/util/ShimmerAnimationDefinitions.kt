4086f764eb49070e3c837c526b37784425b27745
jhjh550 <jhjh550@gmail.com>
2021-10-04 19:48:35 +0900

    Shimmer Loading Animation with Jetpack Compose

---
+++ app/src/main/java/kr/co/mvvmreceipeapp/presentation/components/util/ShimmerAnimationDefinitions.kt
@@ -0,0 +1,2 @@
+package kr.co.mvvmreceipeapp.presentation.components.util
+
--- app/src/main/java/kr/co/mvvmreceipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/kr/co/mvvmreceipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,9 +1,11 @@
 package kr.co.mvvmreceipeapp.presentation.ui.recipe_list
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
@@ -15,11 +17,14 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush.Companion.linearGradient
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
@@ -65,35 +70,48 @@ class RecipeListFragment: Fragment() {
                         onSelectedCategoryChanged = viewModel::onSelectedCategoryChanged,
                         onChangeCategoryScrollPosition = viewModel::onChangeCategoryScrollPosition
                     )
-                    Row(modifier = Modifier
-                        .fillMaxSize()
-                        .height(200.dp),
-                        horizontalArrangement = Arrangement.Center
-                    ){
-                        val state = remember{ mutableStateOf(IDLE)}
 
-                        AnimatedHeartButton(modifier = Modifier, buttonState = state) {
-                            state.value = if(state.value == IDLE) ACTIVE else IDLE
+                    Box(modifier = Modifier.fillMaxWidth()) {
+                        if (loading) {
+                            // LoadingRecipeListShimmer(imageHeight = 250.dp)
                         }
+                        LazyColumn {
+                            itemsIndexed(
+                                items = recipes
+                            ) { index, recipe ->
+                                RecipeCard(recipe) {
+                                }
+                            }
+                        }
+                        CircularIndeterminateProgressBar(isDisplayed = loading)
                     }
-
-
-                    
-//                    PulsingDemo()
-
-//                    Box(modifier = Modifier.fillMaxWidth()){
-//                        LazyColumn{
-//                            itemsIndexed(
-//                                items = recipes
-//                            ){ index, recipe ->
-//                                RecipeCard(recipe) {
-//                                }
-//                            }
-//                        }
-//                        CircularIndeterminateProgressBar(isDisplayed = loading)
-//                    }
                 }
             }
         }
     }
 }
+
+
+@Composable
+fun GradientDemo(){
+    val colors = listOf(Color.Blue, Color.Red, Color.Blue)
+    val brush = linearGradient(
+        colors,
+        start = Offset(200f, 200f),
+        end = Offset(400f, 400f)
+    )
+    Surface(shape = MaterialTheme.shapes.small) {
+        Spacer(modifier = Modifier
+            .fillMaxSize()
+            .background(brush = brush))
+    }
+}
+
+
+
+
+
+
+
+
+
